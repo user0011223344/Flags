3f7d87df0ab0697e3c0c011104a65caa75455d8a
user0011223344 <122041290+user0011223344@users.noreply.github.com>
2023-01-06 18:58:03 +0300

    Update main.py
    Modify and rename functions and vars

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-# данный файл является модификацией исходного
-# потому что библиотеки в исходном проекте старые и при запуске вылезает много ошибок
-# После различных исправлений скрипт запускается, НО его точность страдает
 from pylab import *
 from matplotlib.pyplot import imshow
 import numpy as np
 import os
 import cv2
 import warnings
+import argparse
 
 warnings.simplefilter("ignore")
+
+
+# Change me
+INPUT_FILENAME = "/flags/input_img/6.jpg"
+
+OUTPUT_PDF_FILENAME = "flags.pdf"
+
 
 
 def dist(p1, p2):           # расстояние между точками
     return sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
 
 
-def pers_trans(img, coords):    # преобразование для вырезания
-    pts1 = np.float32(coords)
-    a = coords[0]
-    b = coords[1]
-    d = coords[3]
+def perspective_transformation(img, corner_coords):    # преобразование для вырезания
+    pts1 = np.float32(corner_coords)
+    a = corner_coords[0]
+    b = corner_coords[1]
+    d = corner_coords[3]
     dist1 = dist(a, b)
     dist2 = dist(a, d)
     if dist1 > dist2:
@@ -33,32 +38,32 @@ def pers_trans(img, coords):    # преобразование для вырез
     return dst
 
 
-def draw_rec(cnt):          # определение координат прямоугольника вокруг контура
+def get_rectangle_coords(cnt):          # определение координат прямоугольника вокруг контура
     rect = cv2.minAreaRect(cnt)
     box = cv2.boxPoints(rect)
     box = np.int0(box)
     return box
 
 
-def find(lista, ekstr, ktory):           # поиск крайних вершин
+def find(coords_list, min_or_max_criteria, height_or_width):           # поиск крайних вершин
     index = 0
-    if ekstr == 'max':
-        for i in range(len(lista)):
-            if lista[index][ktory] < lista[i][ktory]:
+    if min_or_max_criteria == 'max':
+        for i in range(len(coords_list)):
+            if coords_list[index][height_or_width] < coords_list[i][height_or_width]:
                 index = i
     else:
-        for i in range(len(lista)):
-            if lista[index][ktory] > lista[i][ktory]:
+        for i in range(len(coords_list)):
+            if coords_list[index][height_or_width] > coords_list[i][height_or_width]:
                 index = i
 
-    return lista[index]
+    return coords_list[index]
 
 
-def corners(points):            # определение крайних вершин
-    a = find(points, 'max', 0)
-    b = find(points, 'max', 1)
-    c = find(points, 'min', 0)
-    d = find(points, 'min', 1)
+def get_corners(coords_list):            # определение крайних вершин
+    a = find(coords_list, 'max', 0)
+    b = find(coords_list, 'max', 1)
+    c = find(coords_list, 'min', 0)
+    d = find(coords_list, 'min', 1)
     return [a, b, c, d]
 
 
@@ -76,25 +81,25 @@ def fun(img, canny_first, canny_second, coef_blur, count_morph):
 
     return contours, hierarchy
 
+# в этой функции извлекаются контуры из входной картинки
+def crop(transformated_img, canny_first, canny_second, coef_blur, count_morph, size):
+    contours, hierarchy = fun(transformated_img, canny_first, canny_second, coef_blur, count_morph) # основная функция для вырезания флага из фотографии
+    hier = hierarchy[0]                          
 
-def crop(img, canny_first, canny_second, coef_blur, count_morph, size):
-    contours, hierarchy = fun(img, canny_first, canny_second, coef_blur, count_morph)
-    hier = hierarchy[0]                          # основная функция для вырезания флага из фотографии
-
-    points = []
-    boxes = []
+    rectangle_coords_list = []
+    boxes_or_list_with_rectangles_coords = []
 
     for i in range(len(hier)):
         if len(contours[i]) > size and hier[i][3] != -1:
             cnt = contours[i]
-            rec = draw_rec(cnt)
-            boxes.append(rec)
-            for p in rec:
-                points.append(p)
+            rectangle_coords = get_rectangle_coords(cnt) # поиск ближайшего к контуру прямоугольника
+            boxes_or_list_with_rectangles_coords.append(rectangle_coords)
+            for p in rectangle_coords:
+                rectangle_coords_list.append(p)
 
-    coords = corners(points)
-    img = pers_trans(img, coords)
-    return img, boxes
+    corners_coords = get_corners(rectangle_coords_list)
+    transformated_img = perspective_transformation(transformated_img, corners_coords)
+    return transformated_img, boxes_or_list_with_rectangles_coords
 
 
 def adjust_gamma(image, gamma=1.0):         # гамма-коррекция
@@ -104,57 +109,88 @@ def adjust_gamma(image, gamma=1.0):         # гамма-коррекция
     return cv2.LUT(image, table)
 
 
-def match(flag, n):           # поиск наилучшего соответствия
-    rows_flag, cols_flag = flag.shape[:2]
+def search_best_match(input_img):           # поиск наилучшего соответствия среди всех etalon-файлов
+    rows_in_input_img, cols_in_input_img = input_img.shape[:2]
     fit = []
-    for i in range(1, n+1):
-        print('baza ' + str(i) + ': ')
-        base = cv2.imread(os.getcwd() + '//flags/base/' + str(i) + '.png')
-        base = cv2.cvtColor(base, cv2.COLOR_RGB2BGR)
-        rows_base, cols_base = base.shape[:2]
-        if rows_flag < rows_base:
-            x = rows_flag
-        else:
-            x = rows_base
-        if cols_flag < cols_base:
-            y = cols_flag
-        else:
-            y = cols_base
-        flag = float64(cv2.resize(flag, (y, x)))
-        base = float64(cv2.resize(base, (y, x)))
+    ETALON_FLAGS_DIR = os.getcwd()+'/flags/etalon_img/'
+    etalon_filenames_list = []
+    for f in os.listdir(ETALON_FLAGS_DIR):
+        full_etalon_filepath = ETALON_FLAGS_DIR + f
+        if os.path.isfile(full_etalon_filepath):
+            etalon_filenames_list.append(full_etalon_filepath)
 
-        fit.append(sum(abs((flag-base))))
-        print(fit[-1])
+    for etalon_filename in etalon_filenames_list:
+            #print('baza ' + str(i) + ': ')
+            etalon_img = cv2.imread(etalon_filename)
+            etalon_img = cv2.cvtColor(etalon_img, cv2.COLOR_RGB2BGR)
+            rows_etalon_img, cols_etalon_img = etalon_img.shape[:2]
+
+            if rows_in_input_img < rows_etalon_img:
+                x = rows_in_input_img
+            else:
+                x = rows_etalon_img
+            if cols_in_input_img < cols_etalon_img:
+                y = cols_in_input_img
+            else:
+                y = cols_etalon_img
+
+            input_img = cv2.resize(input_img, (y, x)).astype("float64")
+            etalon_img = cv2.resize(etalon_img, (y, x)).astype("float64")
+
+            fit.append(sum(abs((input_img-etalon_img))))
+            print(f"Fit with { etalon_filename.split('Flags/flags/')[1] } = {fit[-1]}")
 
     best_fit = min(fit)
-    print('\nbest: ')
-    print(best_fit)
-    idx = fit.index(best_fit)
-    idx = idx + 1
-    return idx
+    found_idx = fit.index(best_fit)
+    found_idx = found_idx + 1
+
+    print(f'\nBest match: {best_fit}')
+    print(f"Found etalon: {etalon_filenames_list[found_idx].split('Flags/flags/')[1]}")
+    print(f"Input filename: {args.input_img}\n\n")
+    return etalon_filenames_list[found_idx]
 
 
 # MAIN
-fig = figure(figsize=(20, 10))
 
-# Тут пишем путь до нашего изображения
-random = cv2.imread(os.getcwd() + '//flags/random/6.jpg')
-random = cv2.cvtColor(random, cv2.COLOR_RGB2BGR)
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input_img',default=INPUT_FILENAME, required=False)
 
+args = parser.parse_args()
+
+# Создание выходного pdf-файла
+output_pdf_file = figure(figsize=(20, 10))
+
+
+input_img = cv2.imread(os.getcwd() + args.input_img)
+# Конвертируем входную картинку в Blue Green Red (потому что opencv так работает)
+input_img = cv2.cvtColor(input_img, cv2.COLOR_RGB2BGR)
+
+# раньше здесь была ошибка
+cutted_and_transformated_img, list_of_boxes_coordinates = crop(input_img, 100, 200, 3, 1, 100)
+cutted_and_transformated_img = adjust_gamma(cutted_and_transformated_img, 0.7)
+
+# добавление строки столбцов в выходной pdf-файл
+# кол-во строк = 1
+# кол-во столбцов = 2
+# индекс = 1
 subplot(1, 2, 1)
+# Добавить входную картинку в выходной pdf-файл
+imshow(input_img)
 
-#error
-result, box = crop(random, 100, 200, 3, 1, 100)
+# главная функция сравнения
+found_etalon_filename = search_best_match(cutted_and_transformated_img)
 
-result = adjust_gamma(result, 0.7)
 
-imshow(random)
 
-idx = match(result, 11)
-base = cv2.imread(os.getcwd() + '//flags/base/' + str(idx) + '.png')
-base = cv2.cvtColor(base, cv2.COLOR_RGB2BGR)
+found_img = cv2.imread(found_etalon_filename)
+found_img = cv2.cvtColor(found_img, cv2.COLOR_RGB2BGR)
 
+# добавление строки столбцов в выходной pdf-файл
+# кол-во строк = 1
+# кол-во столбцов = 2
+# индекс = 2
 subplot(1, 2, 2)
-imshow(base)
+# Добавить эталонную картинку в выходной pdf-файл
+imshow(found_img)
 
-fig.savefig('flags.pdf')
+output_pdf_file.savefig('flags.pdf')
